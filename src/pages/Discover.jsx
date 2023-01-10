b5807975eba54450049b5789ac2f6fa9a5e8919d
nelson chege <nelson3chege@gmail.com>
2023-01-10 11:21:25 +0300

    feat:implimenting music player

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,41 +1,51 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { genres } from '../assets/constants';
+
 import { Error, Loader, SongCard } from '../components';
+import { selectGenreListId } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { genres } from '../assets/constants';
 
-import { useGetTopChatsQuery } from '../redux/services/shazamCore';
-
-export default function Discover() {
+const Discover = () => {
   const dispatch = useDispatch();
-  const {activeSong , isPlaying} = useSelector((state)=> state.player);
-  const { data, isFetching, error } = useGetTopChatsQuery();
-  const genreTitle = 'pop';
-  if (isFetching) return <Loader title="Loadding Songs ..." />;
+  const { genreListId } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading songs..." />;
 
   if (error) return <Error />;
+
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+
         <select
-          onChange={() => {}}
-          value=''
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || 'pop'}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>
       </div>
+
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        { data.map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard
-            key={song.listid}
-            i={i}
+            key={song.key}
+            song={song}
             isPlaying={isPlaying}
             activeSong={activeSong}
-            song={song}
+            data={data}
+            i={i}
           />
         ))}
       </div>
     </div>
   );
-}
+};
+
+export default Discover;
