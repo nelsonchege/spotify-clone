b4ad4899b347a090ed83479e6c37f154dca3b6d9
nelson <nelson3chege@gmail.com>
2023-01-11 01:00:25 +0300

    update: display lyrics

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,9 +12,13 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;
